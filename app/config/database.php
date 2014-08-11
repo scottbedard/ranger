ad5eeb5817e8a1d644211899af91474676fb40c8
Scott Bedard <scottbedard90@gmail.com>
2014-08-10 22:59:15 -0400

    8/10/2014

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'forge',
 			'username'  => 'forge',
-			'password'  => '',
+			'password'  => 'aDAtjtOpiSjPH3PJZWA8',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
